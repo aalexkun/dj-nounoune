cb62417ba45d2fc0d806017636e541a258dc757a
atremblay <34930531+aalexkun@users.noreply.github.com>
2026-05-04 21:05:02 +1000

    Adding reelase script

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  const version = process.env.npm_package_version || 'unknown';
+  Logger.log(`Application version: ${version} is running!`, 'Bootstrap');
 }
 bootstrap();
